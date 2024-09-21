72201ef216cc85aa4a320b503cb439594b06777b
baka4n <qq474899581@gmail.com>
2024-09-22 00:02:19 +0800

    ommit init

---
+++ common/src/main/java/io/github/nitianstudio/ExampleMod.java
@@ -0,0 +1,9 @@
+package io.github.nitianstudio;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "toml_recipe";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
